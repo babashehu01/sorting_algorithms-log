10947ee12d70d017b5ea0adfbc36b584edd0029e
babashehu01 <shettimababashehu57@gmail.com>
2022-07-18 09:51:29 +0100

    Added ascending bubble sort function

---
+++ 0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorts a given array
+ *
+ * @array: input array
+ * @size: sizeof inputed array
+ *
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int swap, flag = 1;
+	size_t i;
+	while(flag)
+	{
+		flag = 0;
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = swap;
+				flag++;
+				print_array(array, size);
+				flag = 1;
+			}
+		}
+	}
+}
